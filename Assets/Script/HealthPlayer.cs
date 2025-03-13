86b5b4c009c108b2fe0054fb8418a8c724c4eed5
LanTN <anhlaan2003@gmail.com>
2025-03-14 00:10:29 +0700

    Buying Armor

--- Assets/Script/HealthPlayer.cs
+++ Assets/Script/HealthPlayer.cs
@@ -3,11 +3,13 @@ using UnityEngine.UI;
 public class HealthPlayer : MonoBehaviour
 {
     [SerializeField] private Slider health;
+    
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     public void UpdatePlayerHealth(float currentValue, float maxValue)
     {
         health.value = currentValue / maxValue;
     }
+    
 
     // Update is called once per frame
     void Update()
